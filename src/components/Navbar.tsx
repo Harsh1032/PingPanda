925962e7a8712a77f14c0d8679252963e25cae42
Harsh <harshchamola@gmail.com>
2025-01-28 13:19:32 +0400

    navbar design completed for large screen devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,74 @@
 import Link from "next/link"
-import { MaxWidthWrapper } from "./MaxWidthWrapper";
-import { SignOutButton } from "@clerk/nextjs";
-import { Button } from "./ui/button";
+import { MaxWidthWrapper } from "./MaxWidthWrapper"
+import { SignOutButton } from "@clerk/nextjs"
+import { Button, buttonVariants } from "./ui/button"
+import { ArrowRight } from "lucide-react"
 
 export const Navbar = () => {
-  const user = true;
+  const user = false;
   return (
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white backdrop-blur-lg transition-all">
-        <MaxWidthWrapper>
-            <div className="flex h-16 items-center justify-between">
-                <Link href="/" className="flex z-40 font-semibold">
-                    Ping<span className="text-brand-700">Panda</span>
+      <MaxWidthWrapper>
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/" className="flex z-40 font-semibold">
+            Ping<span className="text-brand-700">Panda</span>
+          </Link>
+          <div className="h-full flex items-center space-x-4">
+            {user ? (
+              <>
+                <SignOutButton>
+                  <Button className="sm" variant="ghost">
+                    Sign Out
+                  </Button>
+                </SignOutButton>
+
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "sm:flex items-center gap-1",
+                  })}
+                >
+                  Dashboard <ArrowRight className="ml-1.5 size-4" />
                 </Link>
-                <div className="h-full flex items-center space-x-4">
-                  {user ? <> 
-                    <SignOutButton>
-                      <Button>Sign Out</Button>
-                    </SignOutButton>
-                  </> : null}
-                </div>
-            </div>
-        </MaxWidthWrapper>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost"
+                  })}
+                >
+                  Pricing
+                </Link>
+                <Link
+                  href="/sign-in"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost"
+                  })}
+                >
+                  Sign in
+                </Link>
+
+                <div className="h-8 w-px bg-gray-200"/>
+                
+                <Link
+                  href="/sign-up"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "sm:flex items-center gap-1",
+                  })}
+                >
+                  Sign up <ArrowRight className="size-4"/>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
     </nav>
   )
 }
